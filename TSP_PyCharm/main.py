d7ae5cd0876ac4e868986b2e0894dfefe6337b97
tvs111 <73558991+tkachenko0@users.noreply.github.com>
2023-06-09 20:07:04 +0200

    BB 2 types

--- TSP_PyCharm/main.py
+++ TSP_PyCharm/main.py
@@ -1,19 +1,15 @@
 from tsp_solvers import *
 import utils
-from tsp_methods import BranchAndBoundType
+from tsp_solvers import BranchAndBoundType
 
 if __name__ == '__main__':
-    num_vertices = 30
+    num_vertices = 15
 
     costs_matrix = utils.generate_random_cost_matrix(num_vertices)
 
     # costs_matrix = utils.generate_space_matrix()
 
-    # soluzione troppo alta per il bb binario
-    # costs_matrix = [[0, 70, 27, 11, 55, 58, 31, 78], [60, 0, 1, 57, 97, 85, 99, 47], [29, 35, 0, 96, 92, 47, 100, 9], [46, 15, 39, 0, 70, 9, 50, 69], [92, 63, 20, 7, 0, 87, 37, 1], [35, 96, 66, 37, 42, 0, 63, 33], [3, 71, 91, 78, 6, 27, 0, 99], [90, 33, 45, 97, 79, 61, 65, 0]]
-
-    # solve_with_cut_set(costs_matrix, connected=False, visualize=False, print_time=True)
+    # solve_with_cut_set(costs_matrix, connected=True, visualize=False, print_time=True)
     # solve_with_subtour_elimination(costs_matrix, connected=True, visualize=False, print_time=True)
-    # solve_with_branch_and_bound(costs_matrix, bb_type=BranchAndBoundType.BINARY, visualize=False, print_time=True)
-    # solve_with_brute_force(costs_matrix, print_time=True)
+    solve_with_branch_and_bound(costs_matrix, bb_type=BranchAndBoundType.TOTAL, visualize=False, print_time=True)
     solve_with_MTZ(costs_matrix, visualize=False, print_time=True)
--- TSP_PyCharm/tsp_methods.py
+++ TSP_PyCharm/tsp_methods.py
@@ -2,7 +2,7 @@ import itertools
 from docplex.mp.model import Model
 import utils
 from enum import Enum
-
+import heapq
 
 def create_assignment_problem_model(costs_matrix):
     num_vertices = len(costs_matrix)
@@ -75,12 +75,7 @@ def create_subtour_elimination_model(costs_matrix, connected):
     return model
 
 
-class BranchAndBoundType(Enum):
-    BINARY = 0,
-    TOTAL = 1
-
-
-def branch_and_bound(costs_matrix, bb_type):
+def branch_and_bound_total(costs_matrix):
     num_vertices = len(costs_matrix)
 
     best_solution = None
@@ -110,22 +105,78 @@ def branch_and_bound(costs_matrix, bb_type):
             best_objective_value = objective_value
             continue
 
-        count_added_models = 0
         for i, j in itertools.product(range(num_vertices), repeat=2):
             current_var_value = solution.get_var_value(current_model.get_var_by_name(f'x_{i}_{j}'))
             if i != j and current_var_value == 1:
                 new_model = current_model.clone()
                 new_model.add_constraint(new_model.get_var_by_name(f'x_{i}_{j}') == 0)
                 stack.append(new_model)
-
                 nodes_added += 1
-                count_added_models += 1
-                if bb_type == BranchAndBoundType.BINARY and count_added_models == 2:
-                    break
 
     if best_solution is None:
         raise Exception("Infeasible")
 
-    print("\tNodes added", nodes_added)
+    print("\tNodes added:", nodes_added)
+
+    return best_solution
+
+
+def branch_and_bound_binary(costs_matrix):
+    num_vertices = len(costs_matrix)
+
+    best_solution = None
+    best_objective_value = float('inf')
+
+    initial_model, x = create_assignment_problem_model(costs_matrix)
+
+    stack = [initial_model]
+
+    nodes_added = 0
+
+    while stack:
+        current_model = stack.pop(0)
+
+        solution = current_model.solve()
+        if solution is None:
+            continue  # Infeasible solution, backtrack
+
+        objective_value = solution.get_objective_value()
+
+        if objective_value >= best_objective_value:
+            continue  # Solution is worse than the best found so far, backtrack
+
+        if utils.count_subtours(solution, num_vertices) == 1:
+            # Found a feasible solution with lower objective value
+            best_solution = solution
+            best_objective_value = objective_value
+            continue
+
+        # select the most expensive arcs of the current solution
+        max_heap = []
+        for var_name, var_value in solution.iter_var_values():
+            var_name = str(var_name)
+            if var_value == 1:
+                i, j = map(int, var_name.split('_')[1:])
+                current_cost = costs_matrix[i][j]
+                heapq.heappush(max_heap, (-current_cost, i, j))
+
+        # Get the indices of the two most expensive variables
+        (_, i1, j1) = heapq.heappop(max_heap)
+        (_, i2, j2) = heapq.heappop(max_heap)
+
+        left_child = current_model.clone()
+        left_child.add_constraint(left_child.get_var_by_name(f'x_{i1}_{j1}') == 0)
+        stack.append(left_child)
+
+        right_child = current_model.clone()
+        right_child.add_constraint(right_child.get_var_by_name(f'x_{i2}_{j2}') == 0)
+        stack.append(right_child)
+
+        nodes_added += 2
+
+    if best_solution is None:
+        raise Exception("Infeasible")
+
+    print("\tNodes added:", nodes_added)
 
     return best_solution
